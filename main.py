f67077ef9024e5a865b9c179d89c670cbf20f468
avnogy <avner.mindelis@gmail.com>
2023-10-22 18:28:28 +0300

    add main window loop

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import moderngl
 import moderngl_window
 from moderngl_window import WindowConfig, geometry
+
+
+OPENGL_VERSION = (4, 6)
+WINDOW_TITLE = "Slime"
+WINDOW_SIZE = [1920, 1080]
+
+
+class Window(WindowConfig):
+    gl_version = OPENGL_VERSION
+    title = WINDOW_TITLE
+    window_size = WINDOW_SIZE
+    resizable = False
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def render(self, time: float, frame_time: float):
+        self.ctx.clear()
+
+
+if __name__ == "__main__":
+    moderngl_window.run_window_config(Window)
