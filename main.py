c92c099bb3dc8df3f34a94bd598ab8728d6290cb
avnogy <avner.mindelis@gmail.com>
2023-10-24 01:27:53 +0300

    add back randomness and frame time

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ WINDOW_TITLE = "Slime"
 WINDOW_RESOLUTION = [1280, 720]
 SLIME_SHADER_GROUPS = {"X": 1, "Y": 1, "Z": 1}
 SHADERS_DIR = Path(__file__).parent
-AGENTS = 40
+AGENTS = 4000
 
 Agent = np.dtype(
     [('position', 'f4', (2,)), ('angle', 'f4'), ('padding', 'f4')])
@@ -40,19 +40,13 @@ class Window(WindowConfig):
         self.compute_shader = self.load_compute_shader(
             SHADERS_DIR/"slime.comp", SLIME_SHADER_GROUPS)
 
-        # self.compute_shader_frame_time = self.compute_shader["frame_time"]
+        self.compute_shader_frame_time = self.compute_shader["frame_time"]
 
     def init_agents(self):
         agents = np.zeros(AGENTS, dtype=Agent)
-        ps = np.linspace(-1.0, 1.0, AGENTS)
-        for i in range(len(ps)):
-            p = ps[i]
-            agents[i]['position'] = (p, p)
-            agents[i]['angle'] = p
-
-        # agents['position'] = np.random.rand(AGENTS, 2) * 2 - 1
-        # agents['angle'] = np.arctan2(
-            # agents['position'][:, 1], agents['position'][:, 0])
+        agents['position'] = np.random.rand(AGENTS, 2) * 2 - 1
+        agents['angle'] = np.arctan2(
+            agents['position'][:, 1], agents['position'][:, 0])
         return agents
 
     def load_shader(self, shader_filename):
@@ -71,9 +65,9 @@ class Window(WindowConfig):
     def render(self, time: float, frame_time: float):
         self.ctx.clear()
 
-        # frame_time = frame_time if frame_time > 0 else 1 / 60
+        frame_time = frame_time if frame_time > 0 else 1 / 60
 
-        # self.compute_shader_frame_time.value = frame_time
+        self.compute_shader_frame_time.value = frame_time
 
         self.buffer.bind_to_storage_buffer(binding=0)
         self.compute_shader.run(group_x=AGENTS)
