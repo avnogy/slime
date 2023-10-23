0272d04ae5b644043a8ef9665dbcf62a7451f157
avnogy <avner.mindelis@gmail.com>
2023-10-23 19:47:37 +0300

    wip

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+import random
 import moderngl
 import numpy as np
 from pathlib import Path
@@ -8,7 +10,6 @@ from moderngl_window import WindowConfig
 OPENGL_VERSION_NUMBER = (4, 6)
 WINDOW_TITLE = "Slime"
 WINDOW_RESOLUTION = [1280, 720]
-SHADER_FILENAMES = {"vert": "vertex_shader", "frag": "fragment_shader"}
 SLIME_SHADER_GROUPS = {"X": 16, "Y": 1, "Z": 1}
 SHADERS_DIR = Path(__file__).parent
 AGENTS = 25000
@@ -22,41 +23,56 @@ class Window(WindowConfig):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-        self.program = self.ctx.program(**self.load_shaders())
+        shaders = {
+            "vertex_shader": self.load_shader("window.vert"),
+            "fragment_shader": self.load_shader("window.frag")
+        }
+        print(shaders)
+        self.program = self.ctx.program(**shaders)
         self.buffer = self.ctx.buffer(self.init_agents().tobytes())
         self.vao = self.ctx.simple_vertex_array(
             self.program, self.buffer, 'position')
 
+        self.compute_shader = self.load_compute_shader(
+            SHADERS_DIR/"slime.comp", SLIME_SHADER_GROUPS)
+
+        # self.compute_shader_frame_time = self.compute_shader["frame_time"]
+
+        self.slime_groups = tuple(
+            math.ceil(d / l) for d, l in zip((AGENTS, 1, 1), SLIME_SHADER_GROUPS.values()))
+
     def init_agents(self):
-        dtype = np.dtype([
-            ('position', 'f4', (2,)),
-            ('angle', 'f4')
-        ])
-
-        agents = np.zeros(AGENTS, dtype=dtype)
-
+        Agent = np.dtype([('position', 'f4', (2,)), ('angle', 'f4')])
+        agents = np.zeros(AGENTS, dtype=Agent)
         agents['position'] = np.random.rand(AGENTS, 2) * 2 - 1
-        agents['angle'] = np.random.rand(AGENTS) * 2 * np.pi
-
+        agents['angle'] = np.arctan2(
+            agents['position'][:, 1], agents['position'][:, 0])
+        print(agents)
         return agents
 
-    def load_shaders(self):
-        shaders = {}
+    def load_shader(self, shader_filename):
+        shader_source = ""
+        path = SHADERS_DIR / shader_filename
+        if path.exists():
+            with path.open() as file:
+                shader_source = file.read()
+                print(shader_filename, "loaded")
+        else:
+            print(f"Could not find shader: {path}")
+            exit(1)
 
-        for shader, shader_name in SHADER_FILENAMES.items():
-            path = SHADERS_DIR / ("window." + shader)
-            if path.exists():
-                with path.open() as file:
-                    shaders[shader_name] = file.read()
-                    print(shader_name, "loaded")
-            else:
-                print(f"Could not find shader: {path}")
+        return shader_source
 
-        return shaders
-
-    def render(self, time, _):
+    def render(self, time: float, frame_time: float):
         self.ctx.clear()
+
+        # frame_time = frame_time if frame_time > 0 else 1 / 60
+
+        # self.compute_shader_frame_time.value = frame_time
+
+        self.buffer.bind_to_storage_buffer(binding=0)
+        self.compute_shader.run(*self.slime_groups)
+
         self.vao.render(moderngl.POINTS)
 
 
